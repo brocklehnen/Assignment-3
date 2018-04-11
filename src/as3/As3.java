859d37aba5deeabad9c9f7922250232c014764b8
brocklehnen <36985454+brocklehnen@users.noreply.github.com>
2018-04-10 23:36:54 -0600

    Update As3.java

--- src/as3/As3.java
+++ src/as3/As3.java
@@ -5,14 +5,6 @@
  */
 package as3;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 /* “Union-Find Algorithm | Set 2 (Union By Rank and Path Compression).” GeeksforGeeks,
 18 Feb. 2017, www.geeksforgeeks.org/union-find-algorithm-set-2-union-by-rank/.
 This is the source where I found the Union Find Algorithom.
@@ -33,8 +25,11 @@ public class As3 {
 
     /**
      * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+     */ 
+    public static void main(String[] args) 
+    {
+       /*
+
         final Path IN_PATH = FileSystems.getDefault().getPath("input/these.txt");
         Charset charset = Charset.forName("UTF-8");
 
@@ -76,10 +71,37 @@ public class As3 {
         {
             System.err.format("IOException: %s%n", x);
         }
-
-        int N = 1;//vertice
-        int M = 3;//edge
+     */   
+        int N = 4;//vertice
+        int M = 5;//edge
         Table table = new Table(N, M);
-
+        
+// add edge 0-1
+        table.edge[0].start = 0;
+        table.edge[0].end = 1;
+        table.edge[0].weight = 10;
+ 
+        // add edge 0-2
+        table.edge[1].start = 0;
+        table.edge[1].end = 2;
+        table.edge[1].weight = 6;
+ 
+        // add edge 0-3
+        table.edge[2].start = 0;
+        table.edge[2].end = 3;
+        table.edge[2].weight = 5;
+ 
+        // add edge 1-3
+        table.edge[3].start = 1;
+        table.edge[3].end = 3;
+        table.edge[3].weight = 15;
+ 
+        // add edge 2-3
+        table.edge[4].start = 2;
+        table.edge[4].end = 3;
+        table.edge[4].weight = 4;
+ 
+        table.Order();
     }
 }
+
