49d76e1731a88df461735e7e144a5a552b17c623
tanglepelt <36967269+tanglepelt@users.noreply.github.com>
2018-04-10 23:08:22 -0600

    Update Edge.java
    updating authors

--- Edge.java
+++ Edge.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Brittany Couts & Brock Lehnen & Gabby Strong
+//4/10/18
+// The program is for adjacency matrix representation of the graph
+// Will help run Krusals algorithm
 package outlab3.pkg232;
 
-/**
- *
- * @author Britt
- */
 import java.util.TreeSet;
 import java.util.HashSet;
 import java.util.Vector;
