6075ab5c50c82ccac0a0608ce5cc869103aba76e
brocklehnen <36985454+brocklehnen@users.noreply.github.com>
2018-04-10 23:37:24 -0600

    Update Table.java

--- src/as3/Table.java
+++ src/as3/Table.java
@@ -10,7 +10,11 @@ import java.util.Arrays;
 
 
 public class Table {
-    class Edge {
+
+
+
+
+    class Edge implements Comparable<Edge>{
       int start, end, weight;
       public int compareTo(Edge compareEdge){//compares the weight of the edges
           return this.weight-compareEdge.weight;
@@ -28,11 +32,19 @@ public class Table {
         int parent, a;
     }
 
-int find(PriorityQ parent[], int i)//uses compression technique
+           Table( int v, int e){//creates table 
+        M=e;
+        N=v;
+        edge = new Edge[M];
+        for (int i=0;i<e;++i){
+            edge[i] = new Edge();
+        }
+    }
+int find(PriorityQ parents[], int i)//uses compression technique
 {
-    if (parent[i].parent != 1)//find root and makes root the parent of i (PCT)
-       parent[i].parent = find(parent, parent[i].parent);
-    return parent[i].parent;
+    if (parents[i].parent != i)//find root and makes root the parent of i (PCT)
+       parents[i].parent = find(parents, parents[i].parent);
+    return parents[i].parent;
 }
   
 void Union(PriorityQ parent[], int x, int y) // two sets done with the union by rank
@@ -46,21 +58,14 @@ void Union(PriorityQ parent[], int x, int y) // two sets done with the union by 
     else if (parent[xset].a > parent[yset].a){
         parent[yset].parent = xset;
     }
-    else{//if ranks are the same, then combine as one root and increase it's rank by one
+    else{//if the ranks are the same then combine
         parent[yset].parent = xset;
         parent[xset].a++;
     }
 }
     
-       Table( int v, int e){//creates table 
-        M=e;
-        N=v;
-        edge = new Edge[M];
-        for (int i=0;i<e;++i){
-            edge[i] = new Edge();
-        }
-    }
-       void Order()
+
+void Order()
        {
            Edge result[] = new Edge[N];
            int e=0;
@@ -72,13 +77,16 @@ void Union(PriorityQ parent[], int x, int y) // two sets done with the union by 
        Arrays.sort(edge);
        
        PriorityQ parent[] = new PriorityQ[N];
+       for (int i=0; i <N; ++i){
+           parent[i]=new PriorityQ();
+       }
        for (int v =0; v <N; ++v){
            parent[v].parent = v;
            parent[v].a = 0;
        }
        se=0; 
        
-       while (e <N - 1){
+       while (e <N - 1){//Pick the smallest edge
            Edge next_edge = new Edge();
            next_edge = edge[se++];
            
@@ -90,6 +98,14 @@ void Union(PriorityQ parent[], int x, int y) // two sets done with the union by 
                Union(parent, x ,y);
            }
        }
+ 
+ System.out.println("Following are the edges plus the weight at the end");
+ //tells the two points for the shortest path plus the weight at the end
+      int a = 0;
+ for (int i = 0; i < e; ++i){
+            System.out.println(result[i].start+" -- " + result[i].end+" == " + result[i].weight);
+        
+ }
+    }
 
-       }
 }
