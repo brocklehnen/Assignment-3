a82419734419ebc70480e6e513b426afd8565a2d
tanglepelt <36967269+tanglepelt@users.noreply.github.com>
2018-04-10 23:58:07 -0600

    Update As3.java

--- src/as3/As3.java
+++ src/as3/As3.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package outlab3.pkg232;
 
 /* “Union-Find Algorithm | Set 2 (Union By Rank and Path Compression).” GeeksforGeeks,
